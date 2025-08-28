8ed23df9426892bb75ad8413e94240315080c88c
Michael Wilson <michael.a.wilson@yale.edu>
2025-08-28 14:26:11 -0400

    unix permissions

---
+++ scripts/crined_drink/gpt2-crined-dr-optim.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --job-name=gpt2-crined-dr-optim.sh
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --mem=16G
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --time=01-00:00:00
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load miniconda
+
+source activate finetune-lm
+
+echo "Running script scripts/crined_drink/gpt2-crined-dr-optim.sh"
+echo ""
+
+python core/finetune_lm.py \
+	--model_name_or_path gpt2
+	--use_gpu \
+	--train_file "'data/crined_transitive_SVO-OSV_dr_for_human_exp/crined_transitive_SVO-OSV_dr_for_human_exp.txt.gz'" \
+	--validation_file "'data/crined_passive_SVO-OSV_dr_for_human_exp/crined_transitive_SVO-OSV_dr_for_human_exp.txt.gz'" \
+	--patience 30 \
+	--epochs 5000 \
+	--min_epochs 100 \
+	--use_kl_baseline_loss \
+	--kl_dataset datamaker/datasets/miniboki-2022-04-01_22-58-30/miniboki \
+	--do_optimize \
+	--optimize_kwargs.n_trials 100 \
+	--params.lr.values 1e-6 1e-4 \
+	--params.kl_scaleby.values 0 10
