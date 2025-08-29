9ee65ffd8c80169bb6253ca1c8ab66b28f8783ad
Michael Wilson <michael.a.wilson@yale.edu>
2025-08-29 12:43:14 -0400

    update scripts

--- scripts/crined_drink/gpt2-crined-dr-optim.sh
+++ scripts/crined_drink/gpt2-crined-dr-optim.sh
@@ -26,6 +26,7 @@ python core/finetune_lm.py \
 	--use_kl_baseline_loss \
 	--kl_dataset datamaker/datasets/miniboki-2022-04-01_22-58-30/miniboki \
 	--do_optimize \
-	--optimize_kwargs.n_trials 100 \
-	--params.lr.values 1e-6 1e-4 \
-	--params.kl_scaleby.values 0 10
+	--optimize_kwargs.n_trials 500 \
+	--study_kwargs.pruner_kwargs.wrapped_pruner_kwargs.n_startup_steps 0 \
+	--params.lr.values 2e-7 6e-5 \
+	--params.kl_scaleby.values 0.9 2.6
